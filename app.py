bef7bc69b966e568e17648a334d09f49d9b55e43
Tauqeer Ahmed <17sw72@students.muet.edu.pk>
2021-08-10 20:49:41 +0500

    Second API Commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Flutter + Python!'
+    query = str(request.args['query'])
+    return 'Hello Flutter + Python and '+query
 
 
 if __name__ == '__main__':
